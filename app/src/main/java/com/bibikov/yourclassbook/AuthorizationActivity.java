df4235f159a35999d5d5da24fe52149297d397f5
Igor <igorbibikovgggg@gmail.ru>
2021-10-25 15:26:36 +0300

    Initial commit

---
+++ app/src/main/java/com/bibikov/yourclassbook/AuthorizationActivity.java
@@ -0,0 +1,14 @@
+package com.bibikov.yourclassbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AuthorizationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authorization);
+    }
+}
